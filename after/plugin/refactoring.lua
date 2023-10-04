81c6510275e070ecd345b1d33d0ea0444f93deb3
Ayush Juvekar <ayush@pop-os.localdomain>
2023-10-04 08:40:22 +0530

    update config
    change refactoring to builtin
    modify tmux remap

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,9 +1,7 @@
-require('refactoring').setup()
--- load refactoring Telescope extension
-require("telescope").load_extension("refactoring")
-
+-- prompt for a refactor to apply when the remap is triggered
 vim.keymap.set(
-	{"n", "x"},
-	"<leader>rr",
-	function() require('telescope').extensions.refactoring.refactors() end
+    {"n", "x"},
+    "<leader>rr",
+    function() require('refactoring').select_refactor() end
 )
+-- Note that not all refactor support both normal and visual mode
