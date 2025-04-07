db15cdc789351ea055ae01a53557e402636c50be
Ayush Juvekar <ayushjuvekar@gmail.com>
2025-04-07 11:07:16 -0400

    configure codecompanion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -5,4 +5,37 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
+  opts = {
+    adapters = {
+      gemini = function()
+        return require("codecompanion.adapters").extend("gemini", {
+          schema = {
+            model = {
+              default = "gemini-2.5-pro-exp-03-25",
+            },
+          },
+        })
+      end,
+    },
+    display = {
+      action_pallete = {
+        provider = "snacks",
+      },
+    },
+    strategies = {
+      chat = {
+        adapter = "gemini",
+      },
+      inline = {
+        adapter = "gemini",
+      },
+      cmd = {
+        adapter = "gemini",
+      },
+    },
+  },
+  keys = {
+    { "<leader>aa", ":CodeCompanion\n", mode = { "n", "v" }, desc = "Ask AI" },
+    { "<leader>ac", ":CodeCompanionChat\n", mode = { "n", "v" }, desc = "AI Chat" },
+  },
 }
